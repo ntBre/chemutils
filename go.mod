3dc2972d9519c5d1ef12ad49a1846d24795d0c49
Brent Westbrook <brentrwestbrook@gmail.com>
2021-04-27 11:51:36 -0500

    working dummy qsub and molpro, bump go version

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/ntBre/chemutils
 
-go 1.14
+go 1.16
 
 require gonum.org/v1/gonum v0.8.2
--- molpro/molpro.go
+++ molpro/molpro.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bufio"
+	_ "embed"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 var (
@@ -12,7 +17,20 @@ var (
 	procs = flag.Int("t", 1, "dummy flag")
 )
 
+//go:embed test.json
+var js string
+
 func main() {
+	geoms := make(map[string]float64)
+	f := strings.NewReader(js)
+	byts, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(byts, &geoms)
+	if err != nil {
+		panic(err)
+	}
 	flag.Parse()
 	args := flag.Args()
 	if len(args) < 1 {
@@ -23,11 +41,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// TODO grab geometry and maybe whether or not optg is there,
-	// but first step is geometry to match to energy
+	base := args[0][:len(args[0])-len(filepath.Ext(args[0]))]
+	outfile, err := os.Create(base + ".out")
+	defer outfile.Close()
+	if err != nil {
+		panic(err)
+	}
+	// TODO include gradients
 	scanner := bufio.NewScanner(infile)
+	var (
+		geom bool
+		str  strings.Builder
+	)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		fields := strings.Fields(line)
+		switch {
+		case strings.Contains(line, "geometry={"):
+			geom = true
+		case strings.Contains(line, "}") && geom:
+			geom = false
+		case geom && len(fields) == 4:
+			str.WriteString(strings.Join(fields, " ") + "\n")
+		}
 	}
+	val, ok := geoms[str.String()]
+	if !ok {
+		panic("geometry not found")
+	}
+	fmt.Fprintf(outfile, "dummy output\nenergy= %20.12f\n", val)
 }
