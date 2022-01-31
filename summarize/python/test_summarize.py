eea16039ffe5d3bdb26082137c29112cdac4e8a2
Brent Westbrook <brentrwestbrook@gmail.com>
2022-01-31 13:20:13 -0600

    refactor tests and make the recipe verbose

--- summarize/python/test_summarize.py
+++ summarize/python/test_summarize.py
@@ -3,23 +3,37 @@ import numpy as np
 import pandas as pd
 import summarize as summ
 
+pd.options.display.max_columns = None
 
-class TestMethods(unittest.TestCase):
+
+class TestInit(unittest.TestCase):
+    got = summ.Spectro("h2o.json")
+    want = summ.Spectro
+
+    def aslist(self, df, x):
+        return df.deltas[x].tolist()
+
     def compDF(self, a, b: pd.DataFrame, eps: float):
+        "compare two purely numerical DataFrames"
         if np.linalg.norm(a.subtract(b)) > eps:
             self.fail(f"{a} != {b}")
 
-    def test_init(self):
-        want = summ.Spectro
-        want.ZPT = 4656.4369
-        want.freqs = pd.DataFrame(
+    def test_ZPT(self):
+        self.want.ZPT = 4656.4369
+        self.assertEqual(self.got.ZPT, self.want.ZPT)
+
+    def test_freqs(self):
+        self.want.freqs = pd.DataFrame(
             {
                 "harms": pd.Series([3943.69, 3833.702, 1650.933]),
                 "funds": pd.Series([3753.167, 3656.538, 1598.509]),
                 "corrs": pd.Series([3753.1674, 3656.5383, 1598.5086]),
             }
         )
-        want.rots = pd.DataFrame(
+        self.compDF(self.got.freqs, self.want.freqs, 1e-7)
+
+    def test_rots(self):
+        self.want.rots = pd.DataFrame(
             [
                 [27.6557795, 14.5045043, 9.2632043],
                 [26.4993144, 14.4058166, 9.1202295],
@@ -28,14 +42,36 @@ class TestMethods(unittest.TestCase):
             ],
             columns=["A", "B", "C"],
         )
-        #   ],
-        #   "Deltas": [
-        #     34.9642012678,
-        #     761.2706215801,
-        #     -149.5172903186,
-        #     13.9815699759,
-        #     10.9619056097
-        #   ],
+        self.compDF(self.got.rots, self.want.rots, 1e-7)
+
+    def test_deltas(self):
+        self.want.deltas = pd.DataFrame()
+        self.want.deltas["Const."] = [
+            "$\\Delta_{J}$",
+            "$\\Delta_{K}$",
+            "$\\Delta_{JK}$",
+            "$\\delta_{J}$",
+            "$\\delta_{K}$",
+        ]
+        self.want.deltas["Value"] = [
+            34.9642012678,
+            761.2706215801,
+            -149.5172903186,
+            13.9815699759,
+            10.9619056097,
+        ]
+        self.want.deltas["Units"] = ["MHz"] * 5
+
+        self.assertEqual(
+            self.aslist(self.got, "Value"), self.aslist(self.want, "Value")
+        )
+        self.assertEqual(
+            self.aslist(self.got, "Const."), self.aslist(self.want, "Const.")
+        )
+        self.assertEqual(
+            self.aslist(self.got, "Units"), self.aslist(self.want, "Units")
+        )
+
         #   "Phis": [
         #     13799.0093,
         #     2086925.836,
@@ -80,6 +116,3 @@ class TestMethods(unittest.TestCase):
         #     29.61
         #   ]
         # }
-        got = summ.Spectro("h2o.json")
-        self.assertEqual(got.ZPT, want.ZPT)
-        self.compDF(got.freqs, want.freqs, 1e-7)
